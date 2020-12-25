b96914120c46cece4629b45db31cdf7c8e450033
SuoweiHu <suowei.h@gmail.com>
2020-12-25 08:38:47 +0800

    封装📦

---
+++ bin/NetEase/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["NetEase_Crawler","NetEase_Mongodb"]
+from . import NetEase_Crawler
+from . import NetEase_Mongodb
--- bin/main.py
+++ bin/main.py
@@ -1,6 +1,5 @@
-from classes import NetEase_Crawler 
-from classes import NetEase_Mongodb 
-from pprint import pprint
+from NetEase import NetEase_Crawler 
+from NetEase import NetEase_Mongodb 
 import json
 
 def read_fromJson(path):
@@ -10,12 +9,12 @@ def read_fromJson(path):
         return data
 
 def main():
-    database = NetEase_Mongodb.Database()       # 创建一个MongoDBClient的instance
-    database.start()                            # 使用刚刚创建的instance连接database
+    database = NetEase_Mongodb.Database()         # 创建一个MongoDBClient的instance
+    database.start()                              # 使用刚刚创建的instance连接database
     # result = NetEase_Crawler.craw(505508015)    # 爬取用户界面，解析并返回字典结果
-    result = read_fromJson("../src/user/export.json")
-    database.dump_user(result, 505508015)       # 把爬到的内容存进数据库
-    NetEase_Crawler.__driver__.quit()           # 退出浏览器
+    result = read_fromJson("../src/user/export.json") # 这里暂时用本地数据代替
+    database.dump_user(result, 505508015)         # 把爬到的内容存进数据库
+    NetEase_Crawler.__driver__.quit()             # 退出浏览器
     return
 
 if __name__ == "__main__":
